66eeec1b12c78ecd7c5668cd1617a531b0dec920
Connor Roelofsen <connorroelofsen15@gmail.com>
2021-11-09 12:12:32 +0100

    Change html title. -> Convert components to JSX. Remove default images and icons. Modify manifest name and short name.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// Library Imports:
+import React from "react";
+
+// Component Imports:
+
+// Image Imports:
+
+const App = () => {
+  return (
+    <div>
+      Hello from App
+    </div>
+  )
+}
+
+export default App
