8117d9ccceb49bf8026ecf6b011cd4df27359188
Connor Roelofsen <connorroelofsen15@gmail.com>
2021-11-23 17:00:41 +0100

    Change Footer position to absolute. -> Modify MainElbilar CSS, image scale up on hover and height. Create MainKontakt Provkorning and Meddela Oss forms with CSS. Modify index.css, change content-wrapper min-height and padding bottom for absolute footer.

--- src/Components/Main/MainKontakt/MainKontakt.jsx
+++ src/Components/Main/MainKontakt/MainKontakt.jsx
@@ -7,23 +7,358 @@ import React, { Component } from "react";
 // Image Imports:
 
 export default class MainKontakt extends Component {
+  state = {
+    step: 1,
+    status: "Not Submit",
+    name: "",
+    tel: "",
+    email: "",
+    modell: "",
+    aterforsaljare: "",
+    forfragan: "",
+  };
+
+  onClick = (value) => (e) => {
+    const { step } = this.state;
+    this.setState({
+      step: value,
+    });
+  };
 
   render() {
+    // const { step } = this.state;
+    const {
+      step,
+      status,
+      name,
+      tel,
+      email,
+      modell,
+      aterforsaljare,
+      forfragan,
+    } = this.state;
+    const values = { name, tel, email, modell, aterforsaljare, forfragan };
+
+    const handleSubmit = async (e) => {
+      console.log("Triggered form handleSubmit.");
+      e.preventDefault();
+      // this.setState({ step: 9999999 });
+      // const serviceID = "xxx";
+      // const templateID = "xxx_form";
+      // const templateParams = {
+      //   name: values.name,
+      //   tel: values.tel,
+      //   email: values.email,
+      //   modell: values.modell,
+      //   aterforsaljare: values.aterforsaljare,
+      //   forfragan: values.forfragan,
+      // };
+      // window.emailjs.send(serviceID, templateID, templateParams).then(
+      //   function (response) {
+      //     console.log(
+      //       "EmailJS Submit Success.",
+      //       response,
+      //       response.status,
+      //       response.text
+      //     );
+      //   },
+      //   function (error) {
+      //     console.log("EmailJS Submit Failed", error);
+      //   }
+      // );
+    };
 
     return (
       <div className="flexbox-mainkontakt-container">
         <div className="flexbox-mainkontakt-heading-container">
-          <h1 id="mainkontakt-heading">
-            KONTAKTA MIG
-          </h1>
+          <h1 id="mainkontakt-heading">KONTAKTA MIG</h1>
           <p id="mainkontakt-paragraph">
-            Önskar du att provköra eller få mer information om Maxus eldrivna transportbilar, så kan du fylla i formuläret nedan för att bli kontaktad av en av våra återförsäljare.
+            Önskar du att provköra eller få mer information om Maxus eldrivna
+            transportbilar, så kan du fylla i formuläret nedan för att bli
+            kontaktad av en av våra återförsäljare.
           </p>
         </div>
         <div className="flexbox-mainkontakt-form-container">
-          
+          <h3 id="mainkontakt-form-heading">VAD KAN VI HJÄLPA TILL MED?</h3>
+          <div className="flexbox-mainkontakt-form-selection-container">
+            <button
+              onClick={this.onClick("1")}
+              id="mainkontakt-form-selection-button"
+            >
+              Provkörning
+            </button>
+            <button
+              onClick={this.onClick("2")}
+              id="mainkontakt-form-selection-button"
+            >
+              Meddela Oss
+            </button>
+          </div>
+          {(() => {
+            switch (step) {
+              case "1":
+                return (
+                  <div className="flexbox-mainkontakt-form-provkorning">
+                    <form
+                      id="provkorning"
+                      name="provkorning"
+                      onSubmit={handleSubmit}
+                    >
+                      <div className="flexbox-mainkontakt-form-provkorning-label-input-container">
+                        <label htmlFor="modell">VILKEN MODELL?</label>
+                        <select name="modell" id="modell" form="provkorning">
+                          <option value="eDeliver3">e-Deliver 3</option>
+                          <option value="eDeliver9">e-Deliver 9</option>
+                          <option value="eDeliver9Chassis">
+                            e-Deliver 9 Chassis
+                          </option>
+                          <option value="Euniq6">Euniq 6</option>
+                          <option value="EuniqMPV">Euniq MPV</option>
+                          <option value="Euniq5-6-sitsig">
+                            Euniq5-6-sitsig
+                          </option>
+                        </select>
+                      </div>
+                      <div className="flexbox-mainkontakt-form-provkorning-label-input-container">
+                        <label htmlFor="aterforsaljare">
+                          VÄLJ DIN ÅTERFÖRSÄLJARE
+                        </label>
+                        <select
+                          name="aterforsaljare"
+                          id="aterforsaljare"
+                          form="provkorning"
+                        >
+                          <option value="">Mobility Motors Bromma</option>
+                          <option value="">Mobility Motors Sätraf</option>
+                          <option value=""></option>
+                        </select>
+                      </div>
+                      <fieldset>
+                        <legend>KONTAKTINFORMATION</legend>
+                        <div className="flexbox-mainkontakt-information-container">
+                          <label htmlFor="name">Namn</label>
+                          <input
+                            id="name"
+                            name="name"
+                            type="text"
+                            placeholder="Namn"
+                            required
+                          />
+                          <label htmlFor="phone">Telefon</label>
+                          <input
+                            id="phone"
+                            name="phone"
+                            type="number"
+                            placeholder="Telefon"
+                            required
+                          />
+                          <label htmlFor="email">E-postadress</label>
+                          <input
+                            id="email"
+                            name="email"
+                            type="email"
+                            placeholder="E-postadress"
+                            required
+                          />
+                        </div>
+                      </fieldset>
+                      <p id="provkorning-policy-paragraph">
+                        Vi delar inte din e-post med andra och du får bara
+                        information om Maxus Sverige från våra återförsäljare.
+                        Du kan läsa mer om vår integritetspolicy{" "}
+                        <a
+                          id="provkorning-policy-anchor"
+                          href="https://www.mbil.se/om-oss/integritetspolicy"
+                        >
+                          här
+                        </a>
+                        .
+                      </p>
+                      <div className="flexbox-mainkontakt-form-provkorning-villkor-container">
+                        <input id="villkor" name="villkor" type="checkbox" />
+                        <p id="provkorning-villkor-paragraph">
+                          Jag accepterar villkoren för kontaktförfrågan
+                        </p>
+                      </div>
+                      <button
+                        type="submit"
+                        id="provkorning-form-submit-button"
+                        form="provkorning"
+                      >
+                        SKICKA FÖRFRÅGAN
+                      </button>
+                    </form>
+                  </div>
+                );
+              case "2":
+                return (
+                  <div>
+                    <div className="flexbox-mainkontakt-form-provkorning">
+                      <form
+                        id="provkorning"
+                        name="provkorning"
+                        onSubmit={handleSubmit}
+                      >
+                        <div className="flexbox-mainkontakt-form-provkorning-label-input-container">
+                          <label for="forfragan">FÖRFRÅGAN</label>
+                          <textarea id="forfragan" name="forfragan" rows="5" cols="50" />
+                        </div>
+                        <fieldset>
+                          <legend>KONTAKTINFORMATION</legend>
+                          <div className="flexbox-mainkontakt-information-container">
+                            <label htmlFor="name">Namn</label>
+                            <input
+                              id="name"
+                              name="name"
+                              type="text"
+                              placeholder="Namn"
+                              required
+                            />
+                            <label htmlFor="phone">Telefon</label>
+                            <input
+                              id="phone"
+                              name="phone"
+                              type="number"
+                              placeholder="Telefon"
+                              required
+                            />
+                            <label htmlFor="email">E-postadress</label>
+                            <input
+                              id="email"
+                              name="email"
+                              type="email"
+                              placeholder="E-postadress"
+                              required
+                            />
+                          </div>
+                        </fieldset>
+                        <p id="provkorning-policy-paragraph">
+                          Vi delar inte din e-post med andra och du får bara
+                          information om Maxus Sverige från våra återförsäljare.
+                          Du kan läsa mer om vår integritetspolicy{" "}
+                          <a
+                            id="provkorning-policy-anchor"
+                            href="https://www.mbil.se/om-oss/integritetspolicy"
+                          >
+                            här
+                          </a>
+                          .
+                        </p>
+                        <div className="flexbox-mainkontakt-form-provkorning-villkor-container">
+                          <input id="villkor" name="villkor" type="checkbox" />
+                          <p id="provkorning-villkor-paragraph">
+                            Jag accepterar villkoren för kontaktförfrågan
+                          </p>
+                        </div>
+                        <button
+                          type="submit"
+                          id="provkorning-form-submit-button"
+                          form="provkorning"
+                        >
+                          SKICKA FÖRFRÅGAN
+                        </button>
+                      </form>
+                    </div>
+                  </div>
+                );
+              default:
+                return (
+                  <div className="flexbox-mainkontakt-form-provkorning">
+                    <form
+                      id="provkorning"
+                      name="provkorning"
+                      onSubmit={handleSubmit}
+                    >
+                      <div className="flexbox-mainkontakt-form-provkorning-label-input-container">
+                        <label htmlFor="modell">VILKEN MODELL?</label>
+                        <select name="modell" id="modell" form="provkorning">
+                          <option value="eDeliver3">e-Deliver 3</option>
+                          <option value="eDeliver9">e-Deliver 9</option>
+                          <option value="eDeliver9Chassis">
+                            e-Deliver 9 Chassis
+                          </option>
+                          <option value="Euniq6">Euniq 6</option>
+                          <option value="EuniqMPV">Euniq MPV</option>
+                          <option value="Euniq5-6-sitsig">
+                            Euniq5-6-sitsig
+                          </option>
+                        </select>
+                      </div>
+                      <div className="flexbox-mainkontakt-form-provkorning-label-input-container">
+                        <label htmlFor="aterforsaljare">
+                          VÄLJ DIN ÅTERFÖRSÄLJARE
+                        </label>
+                        <select
+                          name="aterforsaljare"
+                          id="aterforsaljare"
+                          form="provkorning"
+                        >
+                          <option value="">Mobility Motors Bromma</option>
+                          <option value="">Mobility Motors Sätraf</option>
+                          <option value=""></option>
+                        </select>
+                      </div>
+                      <fieldset>
+                        <legend>KONTAKTINFORMATION</legend>
+                        <div className="flexbox-mainkontakt-information-container">
+                          <label htmlFor="name">Namn</label>
+                          <input
+                            id="name"
+                            name="name"
+                            type="text"
+                            placeholder="Namn"
+                            required
+                          />
+                          <label htmlFor="phone">Telefon</label>
+                          <input
+                            id="phone"
+                            name="phone"
+                            type="number"
+                            placeholder="Telefon"
+                            required
+                          />
+                          <label htmlFor="email">E-postadress</label>
+                          <input
+                            id="email"
+                            name="email"
+                            type="email"
+                            placeholder="E-postadress"
+                            required
+                          />
+                        </div>
+                      </fieldset>
+                      <p id="provkorning-policy-paragraph">
+                        Vi delar inte din e-post med andra och du får bara
+                        information om Maxus Sverige från våra återförsäljare.
+                        Du kan läsa mer om vår integritetspolicy{" "}
+                        <a
+                          id="provkorning-policy-anchor"
+                          href="https://www.mbil.se/om-oss/integritetspolicy"
+                        >
+                          här
+                        </a>
+                        .
+                      </p>
+                      <div className="flexbox-mainkontakt-form-provkorning-villkor-container">
+                        <input id="villkor" name="villkor" type="checkbox" />
+                        <p id="provkorning-villkor-paragraph">
+                          Jag accepterar villkoren för kontaktförfrågan
+                        </p>
+                      </div>
+                      <button
+                        type="submit"
+                        id="provkorning-form-submit-button"
+                        form="provkorning"
+                      >
+                        SKICKA FÖRFRÅGAN
+                      </button>
+                    </form>
+                  </div>
+                );
+            }
+          })()}
         </div>
       </div>
-    )
+    );
   }
 }
